be2f0fe0d262c45a1b821253f3e077507dd29920
diegoh22 <diegohofficial@gmail.com>
2022-04-08 17:54:15 +0000

    comment area set up

--- codestar/settings.py
+++ codestar/settings.py
@@ -49,12 +49,15 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'cloudinary',
     'blog',
+    'crispy_forms',
     'django_summernote',
 ]
 
 SITE_ID = 1
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
