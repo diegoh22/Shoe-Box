e8453a094e66ef9a47c141d6d63a9b01f3c3781e
diegoh22 <diegohofficial@gmail.com>
2022-04-08 09:16:53 +0000

    comments class fiel commit

--- blog/models.py
+++ blog/models.py
@@ -4,7 +4,7 @@ from cloudinary.models import CloudinaryField
 
 STATUS = ((0, "Draft"), (1, "Published"))
 
-class Post(models.model):
+class Post(models.Model):
     title = models.CharField(max_length=200, unique=True)
     slug = models,SlugField(max_length=200, unique=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="blog_posts")
