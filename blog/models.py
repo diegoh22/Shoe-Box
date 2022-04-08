22ea14d57486f8d43c4512ff3accac817f0b63ec
diegoh22 <diegohofficial@gmail.com>
2022-04-08 16:50:21 +0000

    signup Login and logout commit

--- blog/models.py
+++ blog/models.py
@@ -14,7 +14,7 @@ class Post(models.Model):
     excerpt = models.TextField(blank=True)
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
-    like = models.ManyToManyField(User, related_name='blog_like', blank=True)
+    likes = models.ManyToManyField(User, related_name='blog_like', blank=True)
 
     class Meta:
         ordering = ['-created_on']
